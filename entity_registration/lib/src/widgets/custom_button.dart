afc14f93f78a8db2511b2c5c9709997293315bef
shahsomething <shahsomething@yahoo.com>
2024-07-27 06:06:03 +0500

    Entity registration package added

---
+++ entity_registration/lib/src/widgets/custom_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../constants/app_colors.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    this.child,
+    this.onPressed,
+  });
+  final Widget? child;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primaryColor,
+        foregroundColor: AppColors.whiteColor,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 15,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      child: child,
+    );
+  }
+}
