c5056120a668f6c648ea5368aab38754d748a5ed
shahsomething <shahsomething@yahoo.com>
2024-10-26 16:31:54 +0500

    File picker service added to commons

--- fin_commons/lib/fin_commons.dart
+++ fin_commons/lib/fin_commons.dart
@@ -1,5 +1,6 @@
 library fin_commons;
 
+//Models
 export 'src/models/address.dart';
 export 'src/models/merchant.dart';
 export 'src/models/pay_facs_result.dart';
@@ -10,6 +11,7 @@ export 'src/models/entity_model.dart';
 export 'src/models/service.dart';
 export 'src/models/bank_details.dart';
 
+//Widgets
 export 'src/widgets/custom_button.dart';
 export 'src/widgets/custom_dropdown_field.dart';
 export 'src/widgets/custom_text_field.dart';
@@ -17,7 +19,11 @@ export 'src/widgets/upload_image.dart';
 export 'src/widgets/custom_date_picker.dart';
 export 'src/widgets/custom_appbar.dart';
 
+//Enums
 export 'src/enums/merchant_type.dart';
 export 'src/enums/address_category.dart';
 export 'src/enums/entity.dart';
 export 'src/enums/gender.dart';
+
+//Services
+export 'src/services/file_picker_service.dart';
