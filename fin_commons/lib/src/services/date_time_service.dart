d213c04e71d4f614008470dedf9a7705538382d1
shahsomething <shahsomething@yahoo.com>
2024-11-05 22:59:49 +0500

    Change date format in DateTimeService from MM/dd/yyyy to MM-dd-yyyy

--- fin_commons/lib/src/services/date_time_service.dart
+++ fin_commons/lib/src/services/date_time_service.dart
@@ -2,7 +2,7 @@ import 'package:intl/intl.dart';
 
 class DateTimeService {
   static String mmddyyyyFormat(String date) {
-    var dateFormat = DateFormat("MM/dd/yyyy");
+    var dateFormat = DateFormat("MM-dd-yyyy");
     DateTime? parsedDate = DateTime.tryParse(date);
     if (parsedDate == null) {
       return "-1";
