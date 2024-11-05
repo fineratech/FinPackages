9272d6681af255360a3c32cd32e692ca8fbadb50
shahsomething <shahsomething@yahoo.com>
2024-11-05 22:46:41 +0500

    Updated formating logic

--- fin_commons/lib/src/services/date_time_service.dart
+++ fin_commons/lib/src/services/date_time_service.dart
@@ -3,7 +3,10 @@ import 'package:intl/intl.dart';
 class DateTimeService {
   static String mmddyyyyFormat(String date) {
     var dateFormat = DateFormat("MM/dd/yyyy");
-    DateTime parsedDate = DateTime.parse(date);
+    DateTime? parsedDate = DateTime.tryParse(date);
+    if (parsedDate == null) {
+      return "-1";
+    }
     return dateFormat.format(parsedDate);
   }
 }
