25d9efddd29f8379c7600cc3785e13062b0fa7da
shahsomething <shahsomething@yahoo.com>
2024-12-09 16:10:36 +0500

    Add default case to EntityType switch and update plugin paths

--- entity_registration/lib/src/Screens/entity_registration_screen.dart
+++ entity_registration/lib/src/Screens/entity_registration_screen.dart
@@ -63,6 +63,8 @@ class EntityRegistrationScreen extends StatelessWidget {
         );
       case EntityType.other:
         return Container();
+      default:
+        return Container();
     }
   }
 }
