74dc81f1f96902ebb6e2dcd1a1d786c200987b3a
shahsomething <shahsomething@yahoo.com>
2024-12-10 14:12:49 +0500

    Update Flutter plugin paths to reflect new user directory

--- entity_registration/lib/src/Screens/driver/driver_registration_view_model.dart
+++ entity_registration/lib/src/Screens/driver/driver_registration_view_model.dart
@@ -4,9 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:logger/logger.dart';
 
-class  DriverRegistrationViewModel  extends ChangeNotifier{
-
-  DriverRegistrationViewModel(){
+class DriverRegistrationViewModel extends ChangeNotifier {
+  DriverRegistrationViewModel() {
     initialize();
     _apiFunctionsService = ApiFunctionsService(
       logger: logger,
@@ -14,55 +13,50 @@ class  DriverRegistrationViewModel  extends ChangeNotifier{
   }
 
 //Services
-final Logger logger = Logger();
-late ApiFunctionsService _apiFunctionsService;
+  final Logger logger = Logger();
+  late ApiFunctionsService _apiFunctionsService;
 
 //Form
-final formKey = GlobalKey<FormBuilderState>();
+  final formKey = GlobalKey<FormBuilderState>();
 
+// variables
 
-// variables 
+  String _driverId = ''; // by add_ at start of variable it become private field
+  IdType _idType = IdType.other;
 
-String _driverId = ''; // by add_ at start of variable it become private field
-IdType _idType = IdType.other;
-
-String? _country;
+  String? _country;
   String? _state;
   String? _city;
 
-DateTime ? _driverIdIssueDate ;
-DateTime ? _driverIdExpiryDate ;
+  DateTime? _driverIdIssueDate;
+  DateTime? _driverIdExpiryDate;
 
-DateTime ? _licenseIssueDate ;
-DateTime ? _licenseExpiryDate ;
+  DateTime? _licenseIssueDate;
+  DateTime? _licenseExpiryDate;
 
-IdType _driverIdType =IdType.other;
+  IdType _driverIdType = IdType.other;
 
 // getter
 
-
-String? get country => _country;
+  String? get country => _country;
   String? get state => _state;
   String? get city => _city;
 
-String get driverId => _driverId;
+  String get driverId => _driverId;
 
-IdType get idType => _idType; 
+  IdType get idType => _idType;
 
-IdType get driverIdType => _driverIdType;
+  IdType get driverIdType => _driverIdType;
 
-DateTime? get driverIdIssueDate => _driverIdIssueDate;
+  DateTime? get driverIdIssueDate => _driverIdIssueDate;
 
-DateTime? get driverIdExpiryDate => _driverIdExpiryDate;
-DateTime? get licenseIssueDate => _licenseIssueDate;
-DateTime? get licenseExpiryDate => _licenseExpiryDate;
-
-
+  DateTime? get driverIdExpiryDate => _driverIdExpiryDate;
+  DateTime? get licenseIssueDate => _licenseIssueDate;
+  DateTime? get licenseExpiryDate => _licenseExpiryDate;
 
 // setters
 
-
-set country(String? value) {
+  set country(String? value) {
     _country = value;
     notifyListeners();
   }
@@ -77,144 +71,150 @@ set country(String? value) {
     notifyListeners();
   }
 
-set driverId (String value){
-  _driverId = value;
+  set driverId(String value) {
+    _driverId = value;
 
-  notifyListeners();
-}
+    notifyListeners();
+  }
 
-set idType (IdType value){
-  _idType = value;
+  set idType(IdType value) {
+    _idType = value;
 
-  notifyListeners();
-}
+    notifyListeners();
+  }
 
-set driverIdType (IdType value){
-  _driverIdType = value;
+  set driverIdType(IdType value) {
+    _driverIdType = value;
 
-  notifyListeners();
-}
+    notifyListeners();
+  }
 
-set  driverIdIssueDate(DateTime? value){
-  _driverIdIssueDate = value;
-  notifyListeners();
+  set driverIdIssueDate(DateTime? value) {
+    _driverIdIssueDate = value;
+    notifyListeners();
+  }
 
-}
+  set driverIdExpiryDate(DateTime? value) {
+    _driverIdExpiryDate = value;
+    notifyListeners();
+  }
 
-set  driverIdExpiryDate(DateTime? value){
-  _driverIdExpiryDate = value;
-  notifyListeners();
+  set licenseIssueDate(DateTime? value) {
+    _licenseIssueDate = value;
+    notifyListeners();
+  }
 
-}
-
-set  licenseIssueDate(DateTime? value){
-  _licenseIssueDate = value;
-  notifyListeners();
-
-}
-
-set  licenseExpiryDate(DateTime? value){
-  _licenseExpiryDate = value;
-  notifyListeners();
-
-}
+  set licenseExpiryDate(DateTime? value) {
+    _licenseExpiryDate = value;
+    notifyListeners();
+  }
 
   // controllers
 
-late TextEditingController driverFirstName ;
-late TextEditingController driverLastName ;
-late TextEditingController driverUsername ;
-late TextEditingController driverEmail ;
-late TextEditingController driverPassword ;
-late TextEditingController driverContactNumber ;
-late TextEditingController driverIssuingAuthority ;
-late TextEditingController driverIDType ;
-late TextEditingController driverLicenseType ;
-late TextEditingController driverLicenseCountry ;
-late TextEditingController driverLicenseState ;
-late TextEditingController driverLicenseNumber ;
-late TextEditingController driverLicenseIssueDate ;
-late TextEditingController driverLicenseExpirationDate ;
-late TextEditingController driverIssueCategory ;
-late TextEditingController driverTruckingCompanyId ;
-late TextEditingController driverTypeId ;
-late TextEditingController driverIdNumber ;
+  late TextEditingController driverFirstName;
+  late TextEditingController driverLastName;
+  late TextEditingController driverUsername;
+  late TextEditingController driverEmail;
+  late TextEditingController driverPassword;
+  late TextEditingController driverContactNumber;
+  late TextEditingController driverIssuingAuthority;
+  late TextEditingController driverIDType;
+  late TextEditingController driverLicenseType;
+  late TextEditingController driverLicenseCountry;
+  late TextEditingController driverLicenseState;
+  late TextEditingController driverLicenseNumber;
+  late TextEditingController driverLicenseIssueDate;
+  late TextEditingController driverLicenseExpirationDate;
+  late TextEditingController driverIssueCategory;
+  late TextEditingController driverTruckingCompanyId;
+  late TextEditingController driverTypeId;
+  late TextEditingController driverIdNumber;
 
+// intialize
 
-
-// intialize 
-
-void initialize(){
-
-driverFirstName = TextEditingController();
-driverLastName = TextEditingController();
-driverUsername = TextEditingController();
-driverEmail = TextEditingController();
-driverPassword = TextEditingController();
-driverContactNumber = TextEditingController();
-driverIssuingAuthority = TextEditingController();
-driverIDType = TextEditingController();
-driverLicenseType = TextEditingController();
-driverLicenseCountry = TextEditingController();
-driverLicenseState = TextEditingController();
-driverLicenseNumber = TextEditingController();
-driverLicenseIssueDate = TextEditingController();
-driverLicenseExpirationDate = TextEditingController();
-driverIssueCategory = TextEditingController();
-driverTruckingCompanyId = TextEditingController();
-driverTypeId = TextEditingController();
-driverIdNumber = TextEditingController();
-
-
-}
-
-Future<bool> registerDriver({required String merchantId,
-  required String userId,
-  required EntityType type,
-  required String locationId,})async{
-   int driverId =  await _apiFunctionsService.registerDriver(userId, driverIssueCategory.text, type.name, driverTruckingCompanyId.text, idType.name, driverIdNumber.text, driverIdExpiryDate.toString(), driverIdIssueDate.toString(), country?? "-1", driverLicenseType.text, driverLicenseNumber.text, licenseExpiryDate.toString(), state?? "-1", country??"",);
-   if(driverId==-1){
-    //TODO: show error toast
-    return false;
-   }else{
-    //TODO: show success toast
-    return true;
-   }
-}
-
-// dispose 
-
-@override
-  void dispose() {
-
-
-    driverFirstName.dispose();
-driverLastName.dispose();
-driverUsername.dispose();
-driverEmail.dispose();
-driverPassword.dispose();
-driverContactNumber.dispose();
-driverIssuingAuthority.dispose();
-driverIDType.dispose();
-driverLicenseType.dispose();
-driverLicenseCountry.dispose();
-driverLicenseState.dispose();
-driverLicenseNumber.dispose();
-driverLicenseIssueDate.dispose();
-driverLicenseExpirationDate.dispose();
-driverIssueCategory.dispose();
-driverTruckingCompanyId.dispose();
-driverTypeId.dispose();
-driverIdNumber.dispose();
-
-    
-    super.dispose();
-
-
+  void initialize() {
+    driverFirstName = TextEditingController();
+    driverLastName = TextEditingController();
+    driverUsername = TextEditingController();
+    driverEmail = TextEditingController();
+    driverPassword = TextEditingController();
+    driverContactNumber = TextEditingController();
+    driverIssuingAuthority = TextEditingController();
+    driverIDType = TextEditingController();
+    driverLicenseType = TextEditingController();
+    driverLicenseCountry = TextEditingController();
+    driverLicenseState = TextEditingController();
+    driverLicenseNumber = TextEditingController();
+    driverLicenseIssueDate = TextEditingController();
+    driverLicenseExpirationDate = TextEditingController();
+    driverIssueCategory = TextEditingController();
+    driverTruckingCompanyId = TextEditingController();
+    driverTypeId = TextEditingController();
+    driverIdNumber = TextEditingController();
   }
 
+  Future<bool> registerDriver({
+    required BuildContext context,
+    required String merchantId,
+    required String userId,
+    required EntityType type,
+    required String locationId,
+  }) async {
+    int driverId = await _apiFunctionsService.registerDriver(
+      userId,
+      driverIssueCategory.text,
+      type.name,
+      driverTruckingCompanyId.text,
+      idType.name,
+      driverIdNumber.text,
+      driverIdExpiryDate.toString(),
+      driverIdIssueDate.toString(),
+      country ?? "-1",
+      driverLicenseType.text,
+      driverLicenseNumber.text,
+      licenseExpiryDate.toString(),
+      state ?? "-1",
+      country ?? "",
+    );
+    if (driverId == -1) {
+      if (context.mounted) {
+        Utils.showErrorToast(
+            context: context, message: "Failed to register driver");
+      }
 
+      return false;
+    } else {
+      if (context.mounted) {
+        Utils.showSuccessToast(
+            context: context, message: "Driver Registered Successfully");
+      }
+      return true;
+    }
+  }
 
+// dispose
 
+  @override
+  void dispose() {
+    driverFirstName.dispose();
+    driverLastName.dispose();
+    driverUsername.dispose();
+    driverEmail.dispose();
+    driverPassword.dispose();
+    driverContactNumber.dispose();
+    driverIssuingAuthority.dispose();
+    driverIDType.dispose();
+    driverLicenseType.dispose();
+    driverLicenseCountry.dispose();
+    driverLicenseState.dispose();
+    driverLicenseNumber.dispose();
+    driverLicenseIssueDate.dispose();
+    driverLicenseExpirationDate.dispose();
+    driverIssueCategory.dispose();
+    driverTruckingCompanyId.dispose();
+    driverTypeId.dispose();
+    driverIdNumber.dispose();
 
+    super.dispose();
+  }
 }
