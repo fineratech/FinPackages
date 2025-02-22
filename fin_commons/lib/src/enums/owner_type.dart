378e436eb7750e03f37be2aa3c129a85e5a9fc34
shahsomething <shahsomething@yahoo.com>
2025-02-22 15:04:29 +0500

    Enhance OwnerType enum with string values and improve fromString method for better matching

--- fin_commons/lib/src/enums/owner_type.dart
+++ fin_commons/lib/src/enums/owner_type.dart
@@ -1,13 +1,29 @@
 enum OwnerType {
-  beneficiary,
-  control,
-  other;
+  beneficiary("BeneficialOwner"),
+  control("ControlOwner"),
+  other("Other");
+
+  final String value;
+
+  const OwnerType(this.value);
 
   static OwnerType fromString(String id) {
     switch (id) {
-      case 'beneficiary' || 'Beneficiary' || "BENEFICIARY":
+      case 'beneficiary' ||
+            'Beneficiary' ||
+            "BENEFICIARY" ||
+            "BeneficialOwner" ||
+            "beneficialOwner" ||
+            "BENEFICIALOWNER" ||
+            "beneficialowner":
         return OwnerType.beneficiary;
-      case 'control' || 'Control' || "CONTROL":
+      case 'control' ||
+            'Control' ||
+            "CONTROL" ||
+            "ControlOwner" ||
+            "controlOwner" ||
+            "CONTROLOWNER" ||
+            "controlowner":
         return OwnerType.control;
       case 'other' || 'Other' || 'OTHER':
         return OwnerType.other;
