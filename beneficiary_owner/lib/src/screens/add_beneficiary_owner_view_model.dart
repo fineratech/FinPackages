bc3b8a086e9acfef2185e14caca217b8673622d4
shahsomething <shahsomething@yahoo.com>
2025-02-22 16:31:04 +0500

    Remove commented-out code for owner identity registration in AddBeneficiaryOwnerViewModel

--- beneficiary_owner/lib/src/screens/add_beneficiary_owner_view_model.dart
+++ beneficiary_owner/lib/src/screens/add_beneficiary_owner_view_model.dart
@@ -194,33 +194,33 @@ class AddBeneficiaryOwnerViewModel extends ChangeNotifier {
         postalCodeExtensionController.text,
       );
 
-      if (ownerId != null) {
-        try {
-          await _apiFunctionsService.registerOwnersIssuedIdentity(
-            ownerId,
-            merchantPayFacDbId,
-            idType?.name ?? 'other',
-            idController.text,
-            idIssueCity ?? '-1',
-            idIssueState ?? '-1',
-            idIssueCountry ?? '-1',
-            issueDate!.year.toString(),
-            issueDate!.month.toString(),
-            issueDate!.day.toString(),
-            expiryDate!.year.toString(),
-            expiryDate!.month.toString(),
-            expiryDate!.day.toString(),
-          );
-        } catch (e) {
-          if (context.mounted) {
-            Utils.showErrorToast(
-              context: context,
-              message: "Failed to register Owner's Issued Identity",
-            );
-          }
-          isLoading = false;
-        }
-      }
+      // if (ownerId != null) {
+      //   try {
+      //     await _apiFunctionsService.registerOwnersIssuedIdentity(
+      //       ownerId,
+      //       merchantPayFacDbId,
+      //       idType?.name ?? 'other',
+      //       idController.text,
+      //       idIssueCity ?? '-1',
+      //       idIssueState ?? '-1',
+      //       idIssueCountry ?? '-1',
+      //       issueDate!.year.toString(),
+      //       issueDate!.month.toString(),
+      //       issueDate!.day.toString(),
+      //       expiryDate!.year.toString(),
+      //       expiryDate!.month.toString(),
+      //       expiryDate!.day.toString(),
+      //     );
+      //   } catch (e) {
+      //     if (context.mounted) {
+      //       Utils.showErrorToast(
+      //         context: context,
+      //         message: "Failed to register Owner's Issued Identity",
+      //       );
+      //     }
+      //     isLoading = false;
+      //   }
+      // }
 
       if (context.mounted) {
         Utils.showSuccessToast(
